fb14661366a2ac5029b8fde63bc53491f150aff7
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-05-04 23:22:57 +0200

    Fix hipchat context detection

--- hipchat.js
+++ hipchat.js
@@ -18,7 +18,7 @@ var INFO =
 </plugin>;
 
 function isHipchat() {
-	return /hipchat\.com\/chat/.test(buffer.URL); 
+	return /^https?:\/\/.+\.hipchat\.com\/chat$/.test(buffer.URL); 
 }
 
 function restyle() {
@@ -50,39 +50,40 @@ function focusChat(contents) {
   });
 }
 
-if (isHipchat()) {
+getBrowser().addEventListener("DOMContentLoaded", function() {
+  if (isHipchat()) {
+    restyle();
+  }
+}, true);
 
-  getBrowser().addEventListener("DOMContentLoaded", restyle, true);
-
-  commands.addUserCommand(
-    ['hipchatjumptonext'],
-    'Jump to the next tab',
-    function(){
-      let contents=gBrowser.selectedBrowser.contentDocument;
-      let target=contents.getElementsByClassName('selected')[0].nextSibling || contents.getElementById('tabs').firstChild;
-      jumpTo(contents, target.firstChild);
+commands.addUserCommand(
+  ['hipchatjumptonext'],
+  'Jump to the next tab',
+  function(){
+    let contents=gBrowser.selectedBrowser.contentDocument;
+    let target=contents.getElementsByClassName('selected')[0].nextSibling || contents.getElementById('tabs').firstChild;
+    jumpTo(contents, target.firstChild);
+  }
+);
+commands.addUserCommand(
+  ['hipchatjumptoprevious'],
+  'Jump to the previous tab',
+  function(){
+    let contents=gBrowser.selectedBrowser.contentDocument;
+    let target=contents.getElementsByClassName('selected')[0].previousSibling || contents.getElementById('tabs').lastChild;
+    jumpTo(contents, target.firstChild);
+  }
+);
+commands.addUserCommand(
+  ['hipchatjumptoalert'],
+  'Jump to the next highligthed tab',
+  function(){
+    let contents=gBrowser.selectedBrowser.contentDocument;
+    let targets=contents.getElementsByClassName('alert');
+    if(targets.length<1){
+      return false;
     }
-  );
-  commands.addUserCommand(
-    ['hipchatjumptoprevious'],
-    'Jump to the previous tab',
-    function(){
-      let contents=gBrowser.selectedBrowser.contentDocument;
-      let target=contents.getElementsByClassName('selected')[0].previousSibling || contents.getElementById('tabs').lastChild;
-      jumpTo(contents, target.firstChild);
-    }
-  );
-  commands.addUserCommand(
-    ['hipchatjumptoalert'],
-    'Jump to the next highligthed tab',
-    function(){
-      let contents=gBrowser.selectedBrowser.contentDocument;
-      let targets=contents.getElementsByClassName('alert');
-      if(targets.length<1){
-        return false;
-      }
-      let elem = targets[0].firstChild;
-      jumpTo(contents, elem);
-    }
-  );
-}
+    let elem = targets[0].firstChild;
+    jumpTo(contents, elem);
+  }
+);
